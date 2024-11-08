6ff4a2c0211ea1534183063de3ecfccf7d1a7c6a
Desktop <hikmatilloxon571@gmail.com>
2024-11-08 17:49:08 +0500

    fixed

--- conf/urls.py
+++ conf/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from conf import settings
 
@@ -26,7 +27,7 @@ if settings.DEBUG:
             # license=openapi.License(name="BSD License"),
         ),
         public=True,
-        permission_classes=[],
+        permission_classes=[permissions.IsAdminUser],
     )
 
     import debug_toolbar
